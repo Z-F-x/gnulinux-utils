431821a00e0452fd3baaf30f713301035f094129
Z[F]× <123411327+Z-F-x@users.noreply.github.com>
2025-02-25 17:01:03 +0100

    Create mv_files.sh

---
+++ mv_files.sh
@@ -0,0 +1,35 @@
+# #############################################################################
+#                           File Operations                                   #
+# #############################################################################
+
+# Alias for mvfiles
+alias move-files="mvfiles"
+alias move_files="mvfiles"
+alias mv-files="mvfiles"
+
+# Move all files (not folders) in current directory to path  
+mvfiles() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: movefiles <destination_folder>"
+    return 1
+  fi
+  find . -maxdepth 1 -type f -exec mv {} "$1" \;
+}
+
+# Alias for mvall
+alias move-all="mvall"
+alias mv-all="mvall"
+alias move_all="mvall"
+
+# Move all files and folders in current directory to path
+mvall() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: moveall <destination_folder>"
+    return 1
+  fi
+  mv -- * "$1"
+}
+
+# #############################################################################
+#                          File System Navigation                             #
+# #############################################################################
